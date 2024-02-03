c9776840cc665d1b7d190329a0dfc9ec9f8f7edb
Ximena <laxivasco@gmail.com>
2024-02-02 20:40:33 -0500

    Creacion del servidor basico con express

---
+++ Backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port 3001");
+});
